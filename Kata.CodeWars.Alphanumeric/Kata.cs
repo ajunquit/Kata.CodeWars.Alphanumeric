b871825f7275aa73a98a012f6d5dbd59acb5a0f4
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-08-28 22:11:46 -0500

    Add project files.

---
+++ Kata.CodeWars.Alphanumeric/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.CodeWars.Alphanumeric
+{
+    public class Kata
+    {
+        public static bool Alphanumeric(string str)
+        {
+            return str.All(x => char.IsLetterOrDigit(x)) && !string.IsNullOrEmpty(str);
+        }
+    }
+}
